052ecde37e0030a43375a5032383fa38e90d2600
Usman Zaheer <usmanzaheer1995@gmail.com>
2024-01-09 16:51:04 +0100

    add flag for getting words in a file. add flag for character count, but it needs more work for locale

--- src/main/kotlin/ccwc/Main.kt
+++ src/main/kotlin/ccwc/Main.kt
@@ -8,7 +8,6 @@ import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
 
-
 fun main(args: Array<String>) {
     App().main(args)
 }
@@ -16,6 +15,8 @@ fun main(args: Array<String>) {
 class App : CliktCommand() {
     val numOfBytes by option("-c", "--numOfBytes", help = "Number of bytes to read").flag()
     val numOfLines by option("-l", help = "Number of lines to read").flag()
+    val numOfWords by option("-w", help = "Number of words to read").flag()
+    val numOfCharacters by option("-m", help = "Number of characters to read").flag()
     val filename by argument()
 
     override fun run() {
@@ -28,20 +29,39 @@ class App : CliktCommand() {
         if (numOfBytes) {
             println("Total bytes: ${readNumOfBytes(file)}")
         }
-        
+
         if (numOfLines) {
             println("Total lines: ${readNumOfLines(file)}")
+        }
+
+        if (numOfWords) {
+            println("Total words: ${readNumOfWords(file)}")
+        }
+
+        if (numOfCharacters) {
+            println("Total characters: ${readNumOfCharacters(file)}")
         }
     }
 
     private fun readNumOfBytes(file: File): Int {
         return file.readBytes().size
     }
+
     private fun readNumOfLines(file: File): Int {
         val reader = BufferedReader(FileReader(file))
         var lines = 0
         while (reader.readLine() != null) lines++
 
         return lines
+    }
+
+    private fun readNumOfWords(file: File): Int {
+        val text = file.readText().trim()
+        return text.split("\\s+".toRegex()).size
+    }
+
+    private fun readNumOfCharacters(file: File): Int {
+        val text = file.readText()
+        return text.length
     }
 }
